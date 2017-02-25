b8a34e5bc55dcf27d2777807f9daac3b684a81cf
DaMatrix <joey.rabil@gmail.com>
2017-02-25 23:29:06 +0100

    Add ALL PC packets to translator lists
    Note to self: remove the client -> server packets from this list

--- src/main/java/net/pocketdreams/sequinland/net/translator/TranslatorRegistry.java
+++ src/main/java/net/pocketdreams/sequinland/net/translator/TranslatorRegistry.java
@@ -1,6 +1,8 @@
 package net.pocketdreams.sequinland.net.translator;
 
 import com.flowpowered.network.Message;
+import net.glowstone.net.message.KickMessage;
+import net.glowstone.net.message.SetCompressionMessage;
 import net.glowstone.net.message.handshake.HandshakeMessage;
 import net.glowstone.net.message.login.EncryptionKeyRequestMessage;
 import net.glowstone.net.message.login.EncryptionKeyResponseMessage;
@@ -63,6 +65,46 @@ import net.glowstone.net.message.play.game.UpdateSignMessage;
 import net.glowstone.net.message.play.game.UserListHeaderFooterMessage;
 import net.glowstone.net.message.play.game.UserListItemMessage;
 import net.glowstone.net.message.play.game.WorldBorderMessage;
+import net.glowstone.net.message.play.inv.CloseWindowMessage;
+import net.glowstone.net.message.play.inv.CreativeItemMessage;
+import net.glowstone.net.message.play.inv.EnchantItemMessage;
+import net.glowstone.net.message.play.inv.HeldItemMessage;
+import net.glowstone.net.message.play.inv.OpenWindowMessage;
+import net.glowstone.net.message.play.inv.SetWindowContentsMessage;
+import net.glowstone.net.message.play.inv.SetWindowSlotMessage;
+import net.glowstone.net.message.play.inv.TransactionMessage;
+import net.glowstone.net.message.play.inv.WindowClickMessage;
+import net.glowstone.net.message.play.inv.WindowPropertyMessage;
+import net.glowstone.net.message.play.player.BlockPlacementMessage;
+import net.glowstone.net.message.play.player.BossBarMessage;
+import net.glowstone.net.message.play.player.CameraMessage;
+import net.glowstone.net.message.play.player.ClientStatusMessage;
+import net.glowstone.net.message.play.player.CombatEventMessage;
+import net.glowstone.net.message.play.player.DiggingMessage;
+import net.glowstone.net.message.play.player.InteractEntityMessage;
+import net.glowstone.net.message.play.player.PlayerAbilitiesMessage;
+import net.glowstone.net.message.play.player.PlayerActionMessage;
+import net.glowstone.net.message.play.player.PlayerLookMessage;
+import net.glowstone.net.message.play.player.PlayerPositionLookMessage;
+import net.glowstone.net.message.play.player.PlayerPositionMessage;
+import net.glowstone.net.message.play.player.PlayerSwingArmMessage;
+import net.glowstone.net.message.play.player.PlayerUpdateMessage;
+import net.glowstone.net.message.play.player.ResourcePackSendMessage;
+import net.glowstone.net.message.play.player.ResourcePackStatusMessage;
+import net.glowstone.net.message.play.player.ServerDifficultyMessage;
+import net.glowstone.net.message.play.player.SpectateMessage;
+import net.glowstone.net.message.play.player.SteerVehicleMessage;
+import net.glowstone.net.message.play.player.TabCompleteMessage;
+import net.glowstone.net.message.play.player.TabCompleteResponseMessage;
+import net.glowstone.net.message.play.player.TeleportConfirmMessage;
+import net.glowstone.net.message.play.player.UseBedMessage;
+import net.glowstone.net.message.play.scoreboard.ScoreboardDisplayMessage;
+import net.glowstone.net.message.play.scoreboard.ScoreboardObjectiveMessage;
+import net.glowstone.net.message.play.scoreboard.ScoreboardScoreMessage;
+import net.glowstone.net.message.play.scoreboard.ScoreboardTeamMessage;
+import net.glowstone.net.message.status.StatusPingMessage;
+import net.glowstone.net.message.status.StatusRequestMessage;
+import net.glowstone.net.message.status.StatusResponseMessage;
 import net.marfgamer.jraknet.RakNetPacket;
 import net.pocketdreams.sequinland.net.protocol.packets.BatchPacket;
 import net.pocketdreams.sequinland.net.protocol.packets.LoginPacket;
@@ -94,7 +136,8 @@ public final class TranslatorRegistry {
     public static HashMap<Class<? extends RakNetPacket>, PEToPCTranslator> PE_TO_PC_TRANSLATORS = new HashMap<>();
 
     static {
-        // =======[ NOTCHIAN TO POCKET ]=======
+        // -----=====≡≡≡≡≡ BEGIN NOTCHIAN TO POCKET ≡≡≡≡≡=====-----
+
         // Handshake
         PC_TO_PE_TRANSLATORS.put(HandshakeMessage.class, new IgnorePacketTranslator());
 
@@ -104,7 +147,8 @@ public final class TranslatorRegistry {
         PC_TO_PE_TRANSLATORS.put(LoginStartMessage.class, new IgnorePacketTranslator());
         PC_TO_PE_TRANSLATORS.put(LoginSuccessMessage.class, new IgnorePacketTranslator());
 
-        // Play
+        // --== Begin play packets ==--
+
         // Entitiy
         PC_TO_PE_TRANSLATORS.put(AnimateEntityMessage.class, new IgnorePacketTranslator());
         PC_TO_PE_TRANSLATORS.put(AttachEntityMessage.class, new IgnorePacketTranslator());
@@ -166,12 +210,72 @@ public final class TranslatorRegistry {
         PC_TO_PE_TRANSLATORS.put(UserListItemMessage.class, new IgnorePacketTranslator());
         PC_TO_PE_TRANSLATORS.put(WorldBorderMessage.class, new IgnorePacketTranslator());
 
-        // TODO: The rest of the packets
-        // =======[ POCKET TO NOTCHIAN ]=======
-        PE_TO_PC_TRANSLATORS.put(LoginPacket.class, new PELoginPacketTranslator());
+        // Inventory
+        PC_TO_PE_TRANSLATORS.put(CloseWindowMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(CreativeItemMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(EnchantItemMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(HeldItemMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(OpenWindowMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(SetWindowContentsMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(SetWindowSlotMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(TransactionMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(WindowClickMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(WindowPropertyMessage.class, new IgnorePacketTranslator());
+
+        // Player
+        PC_TO_PE_TRANSLATORS.put(BlockPlacementMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(BossBarMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(CameraMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ClientStatusMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(CombatEventMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(DiggingMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(InteractEntityMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerAbilitiesMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerActionMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerLookMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerPositionLookMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerPositionMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerSwingArmMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(PlayerUpdateMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ResourcePackSendMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ResourcePackStatusMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ServerDifficultyMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(SpectateMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(SteerVehicleMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(TabCompleteMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(TabCompleteResponseMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(TeleportConfirmMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(UseBedMessage.class, new IgnorePacketTranslator());
+
+        // Scoreboard
+        PC_TO_PE_TRANSLATORS.put(ScoreboardDisplayMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ScoreboardObjectiveMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ScoreboardScoreMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(ScoreboardTeamMessage.class, new IgnorePacketTranslator());
+
+        // --== End play packets ==--
+
+        // Status
+        PC_TO_PE_TRANSLATORS.put(StatusPingMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(StatusRequestMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(StatusResponseMessage.class, new IgnorePacketTranslator());
+
+        // Misc
+        PC_TO_PE_TRANSLATORS.put(KickMessage.class, new IgnorePacketTranslator());
+        PC_TO_PE_TRANSLATORS.put(SetCompressionMessage.class, new IgnorePacketTranslator());
+
+        //TODO: remove client -> server packets from PC to PE translators
+        // -----=====≡≡≡≡≡ END NOTCHIAN TO POCKET ≡≡≡≡≡=====-----
+
+        // -----=====≡≡≡≡≡ BEGIN POCKET TO NOTCHIAN ≡≡≡≡≡=====-----
         PE_TO_PC_TRANSLATORS.put(BatchPacket.class, new PEBatchPacketTranslator());
+        PE_TO_PC_TRANSLATORS.put(LoginPacket.class, new PELoginPacketTranslator());
+        PE_TO_PC_TRANSLATORS.put(MovePlayerPacket.class, new PEMovePlayerPacketTranslator());
         PE_TO_PC_TRANSLATORS.put(TextPacket.class, new PETextPacketTranslator());
         PE_TO_PC_TRANSLATORS.put(RequestChunkRadiusPacket.class, new PERequestChunkRadiusPacketTranslator());
-        PE_TO_PC_TRANSLATORS.put(MovePlayerPacket.class, new PEMovePlayerPacketTranslator());
+
+        // -----=====≡≡≡≡≡ END POCKET TO NOTCHIAN ≡≡≡≡≡=====-----
+
+        // TODO: The rest of the packets
     }
 }
